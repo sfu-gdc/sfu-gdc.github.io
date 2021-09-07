a56874ae7a65bc564d03cf51b7841161b4cc443c
Caleb Whitehead <whiitehead@gmail.com>
2021-09-07 14:58:18 -0700

    add some stuff from the tutorial

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+const AboutPage = () => {
+  return (
+    <main>
+      <title>About gamedev club</title>
+      <h1>About gamedev club</h1>
+      <p>something about the club</p>
+    </main>
+  )
+}
+
+export default AboutPage
