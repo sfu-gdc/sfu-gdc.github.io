01f1619a3ea042a6a0b1171f3bd105d20c5efe0b
Gabe <24978329+EarthenSky@users.noreply.github.com>
2022-01-13 17:43:12 -0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://sfugamedev.club",
     title: "sfugamedev",
   },
   pathPrefix:"/sfu-gdc.github.io",
