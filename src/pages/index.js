6a28b7b992ffbe7b66eafd5e33d8996463308566
Alex <metroid3d2@gmail.com>
2022-07-04 22:34:36 -0700

    New website first commit - Landing page, links and a video background!

--- src/pages/index.js
+++
@@ -1,9 +0,0 @@
-import * as React from "react"
-import Button from "@material-ui/core/Button"
-import Layout from "../components/Layout"
-
-const IndexPage = () => {
-  return <Layout at="home"></Layout>
-}
-
-export default IndexPage
